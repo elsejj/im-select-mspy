72393af3f8aca1e2e5a405b7b7e9b34c7cf3fa5b
LiJia <lijia@aifantai.com>
2023-03-13 10:20:07 +0800

    more configurable

--- main.cc
+++ main.cc
@@ -1,13 +1,18 @@
+
+#define UNICODE
+
 #include <Windows.h>
 #include <UIAutomation.h>
 #include <iostream>
 #include <string>
 #include <comip.h>
+#include <comdef.h>
 #include <vector>
 #include <regex>
 #include <WinUser.h>
 #include <cctype>
 #include <algorithm>
+#include <locale>
 
 using namespace std;
 
@@ -18,62 +23,82 @@ typedef _com_ptr_t<_com_IIID<IUIAutomationCondition, &__uuidof(IUIAutomationCond
 typedef _com_ptr_t<_com_IIID<IUIAutomationElementArray, &__uuidof(IUIAutomationElementArray)>> IUIAutomationElementArrayPtr;
 typedef _com_ptr_t<_com_IIID<IUIAutomationInvokePattern, &__uuidof(IUIAutomationInvokePattern)>> IUIAutomationInvokePatternPtr;
 
-const wchar_t * TaskBarName = L"任务栏";
-const char * ImeNamePrefix = "托盘输入指示器";
 
-string get_element_name(IUIAutomationElementPtr pElement)
+// command line options
+struct CliOptions
+{
+  // no prefix
+  wstring mode;
+  // -k= 
+  wstring switch_keys;
+  // -t=
+  wstring taskbar_name;
+  // -i=
+  wstring ime_capture_re;
+
+  wregex ime_capture;
+};
+
+// ime button in taskbar
+struct ImeButton
+{
+  wstring current_mode;
+  IUIAutomationElementPtr pElement;
+};
+
+wstring get_element_name(IUIAutomationElementPtr pElement)
 {
   _bstr_t name;
   pElement->get_CurrentName(name.GetAddress());
   if (name.length() > 0)
   {
-    return string((const char*)name);
+    return wstring((const wchar_t*)name);
   }
   else
   {
-    return "";
+    return L"";
   }
 }
 
-vector<string> split_string(const string & str, const string & delim)
+vector<wstring> split_string(const wstring & str, const wstring & delim)
 {
   vector<string> result;
-  regex re(delim);
-  sregex_token_iterator first{ str.begin(), str.end(), re, -1 }, last;
+  wregex re(delim);
+  wsregex_token_iterator first{ str.begin(), str.end(), re, -1 }, last;
   return { first, last };
 }
 
-SHORT vk_from_text(const string & text) {
-  if (text == "shift") {
+SHORT vk_from_text(const wstring & text) {
+  if (text == L"shift") {
     return VK_SHIFT;
   }
-  if (text == "ctrl") {
+  if (text == L"ctrl") {
     return VK_CONTROL;
   }
-  if (text == "alt") {
+  if (text == L"alt") {
     return VK_MENU;
   }
-  if (text == "win") {
+  if (text == L"win") {
     return VK_LWIN;
   }
-  if (text == "space") {
+  if (text == L"space") {
     return VK_SPACE;
   }
   return 0;
 }
 
-vector<INPUT> get_input_from_string(string str)
+vector<INPUT> get_input_from_string(wstring str)
 {
   vector<INPUT> result;
   transform(str.begin(), str.end(), str.begin(), ::tolower);
-  auto keys = split_string(str, "\\+");
-  transform(keys.begin(), keys.end(), back_insert_iterator(result), [](const string & key) {
+  auto keys = split_string(str, L"\\+");
+  transform(keys.begin(), keys.end(), back_insert_iterator(result), [](const wstring & key) {
     INPUT input = { 0 };
     input.type = INPUT_KEYBOARD;
     input.ki.wVk = vk_from_text(key);
     return input;
   });
-  transform(keys.rbegin(), keys.rend(), back_insert_iterator(result), [](const string & key) {
+  transform(keys.rbegin(), keys.rend(), back_insert_iterator(result), [](const wstring & key) {
     INPUT input = { 0 };
     input.type = INPUT_KEYBOARD;
     input.ki.wVk = vk_from_text(key);
@@ -84,23 +109,21 @@ vector<INPUT> get_input_from_string(string str)
 }
 
 
-struct ImeButton
-{
-  string current_mode;
-  IUIAutomationElementPtr pElement;
-};
 
 
-ImeButton get_ime_button(IUIAutomationPtr pAutomation) {
+ImeButton get_ime_button(const CliOptions & options) {
+  IUIAutomationPtr pAutomation;
   IUIAutomationElementPtr pDesktop;
   IUIAutomationElementPtr pTaskBar;
+  IUIAutomationConditionPtr pCondition;
+
+  auto hr = pAutomation.CreateInstance(CLSID_CUIAutomation);
 
   pAutomation->GetRootElement(&pDesktop);
 
-  IUIAutomationConditionPtr pCondition;
-  pAutomation->CreatePropertyCondition(UIA_NamePropertyId, _variant_t(TaskBarName), &pCondition);
+  pAutomation->CreatePropertyCondition(UIA_NamePropertyId, _variant_t(options.taskbar_name.c_str()), &pCondition);
 
-  pDesktop->FindFirst(TreeScope_Children, pCondition, &pTaskBar);
+  hr = pDesktop->FindFirst(TreeScope_Children, pCondition, &pTaskBar);
 
   pAutomation->CreatePropertyCondition(UIA_ControlTypePropertyId, _variant_t(UIA_ButtonControlTypeId), &pCondition);
 
@@ -115,53 +138,113 @@ ImeButton get_ime_button(IUIAutomationPtr pAutomation) {
     arrButtons->GetElement(i, &pButton);
     auto name = get_element_name(pButton);
 
-    if (name.find(ImeNamePrefix) == 0)
-    {
-      auto parts = split_string(name, "\\s");
-      return { parts[1], pButton };
+    wsmatch match;
+    if (regex_search(name, match, options.ime_capture)) {
+      return { match[1], pButton };
     }
   }
-  return { "", nullptr };
+  return { L"", nullptr };
+}
+
+// default chinese options
+CliOptions chinese_options()
+{
+  CliOptions options;
+  options.taskbar_name = L"任务栏";
+  options.ime_capture_re = L"托盘输入指示器\\s+(\\w+)"; //\\s+(\\S+)\\s*.+";
+  options.switch_keys = L"shift";
+  return options;
+}
+
+// parse command line options
+CliOptions parse_options(int argc, wchar_t * argv[])
+{
+  CliOptions options = chinese_options();
+  for (int i = 1; i < argc; i++)
+  {
+    auto arg = argv[i];
+    if (arg[0] == L'-')
+    {
+      auto pos = wcschr(arg, L'=');
+      if (pos)
+      {
+        auto key = wstring(arg + 1, pos);
+        auto value = wstring(pos + 1);
+        if (key == L"k")
+        {
+          options.switch_keys = value;
+        }
+        else if (key == L"t")
+        {
+          options.taskbar_name = value;
+        }
+        else if (key == L"i")
+        {
+          options.ime_capture_re = value;
+        }
+      }
+    }
+    else
+    {
+      options.mode = arg;
+    }
+  }
+  if (options.ime_capture_re.length() > 0)
+  {
+    options.ime_capture = wregex(options.ime_capture_re);
+  }
+  return options;
+}
+
+void print_options(const CliOptions & options)
+{
+  wcout << L"taskbar name: " << options.taskbar_name << endl;
+  wcout << L"ime capture: " << options.ime_capture_re << endl;
+  wcout << L"switch keys: " << options.switch_keys << endl;
+  wcout << L"mode: " << options.mode << endl;
 }
 
 
-
-int main(int argc, char * argv[])
+int wmain(int argc, wchar_t * argv[])
 {
+  std::ios::sync_with_stdio(false);
+  std::locale::global( std::locale("") );
+
+
+  auto options = parse_options(argc, argv);
+
+  //print_options(options);
+
   CoInitialize(NULL);
 
-  IUIAutomationPtr pAutomation;
+  ImeButton ime_button;
 
-  auto hr = pAutomation.CreateInstance(CLSID_CUIAutomation);
-
-  if (FAILED(hr))
+  try {
+    ime_button = get_ime_button(options);
+  }
+  catch (_com_error&  e)
   {
+    wcout << L"get ime button failed: " << e.ErrorMessage() << endl;
     return 1;
   }
-
-  auto ime_button = get_ime_button(pAutomation);
 
   if (!ime_button.pElement)
   {
     return 1;
   }
 
-  if (argc == 1)
+
+  if (options.mode.empty())
   {
     // get current mode
-    cout << ime_button.current_mode << endl;
+    wcout << ime_button.current_mode << endl;
   }
   else
   {
     // do switch
-    auto mode = argv[1];
-    if (mode != ime_button.current_mode)
+    if (options.mode != ime_button.current_mode)
     {
-      const char * keys = "shift";
-      if (argc > 2) {
-        keys = argv[2];
-      }
-      auto input = get_input_from_string(keys);
+      auto input = get_input_from_string(options.switch_keys);
       SendInput((UINT)input.size(), input.data(), sizeof(input[0]));
     }
   }
